cc4135ba4a3553d3075fc397e4ab660ad1609ca9
Ben Paulson <email.benpaulson@gmail.com>
2023-08-14 21:14:29 -0500

    Made form that doesnt work

--- py/submit_post_form.py
+++ py/submit_post_form.py
@@ -1,16 +1,36 @@
-import csv
-import cgi
+# import csv
+# import cgi
 
-form = cgi.FieldStorage()
+# form = cgi.FieldStorage()
 
-title = form.getvalue("title")
-content = form.getvalue("content")
+# title = form.getvalue("title")
+# content = form.getvalue("markdownInput")
+# thumbnail = form.getvalue("thumbnail")
+# summary = form.getvalue("summary")
+# category = form.getvalue("category")
+# author = form.getvalue("author")
+# email = form.getvalue("email")
 
-if title and content:
-    with open("posts.csv", "a", newline="") as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([title, content])
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print(request.method)
+    if request.method == 'POST':
+        # Call your Python script here
+        print("AHHHHHHHH")
+    return render_template('maic/index.html')
+
+
+
+# if title and content and thumbnail and summary and category and author:
+#     print([title, content, thumbnail, summary, category, author, email])
+#     with open("posts.csv", "a", newline="") as csvfile:
+#         csvwriter = csv.writer(csvfile)
+#         csvwriter.writerow([title, content, thumbnail, summary, category, author, email])
         
-    print("Content submitted successfully.")
-else:
-    print("Please provide title and content.")
+#     print("Content submitted successfully.")
+# else:
+#     print("Please provide all the required fields...")
