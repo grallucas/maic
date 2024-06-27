b256ed40b2fa23e4e5355088a65948067c24c9f6
Andy Dao <andydao2020@gmail.com>
2024-06-27 17:06:05 -0500

    learning tree refactor

---
+++ pages/src/LearningTree.tsx
@@ -0,0 +1,24 @@
+import logo from "./logo.png";
+
+const LearningTree = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/LearningTree.js</code> and save to reload LearningTree.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default LearningTree;
