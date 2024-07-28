b19753590cb64a9d10f1d6ad984cf646c2a629e5
tygeplater <platert@msoe.edu>
2024-07-28 16:19:48 -0500

    Got custom node working

--- pages/src/components/library/LearningTreeNode.tsx
+++ pages/src/components/library/LearningTreeNode.tsx
@@ -2,6 +2,19 @@ import React, { memo } from 'react';
 import { Handle, Position } from '@xyflow/react';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Stack from '@mui/material/Stack';
+import type { Node, NodeProps } from '@xyflow/react';
+ 
+type treeNode = Node<{ 
+    name: string;
+    image_path: string;
+    description: string;
+    category: string;
+    category_color: string;
+    highlighted_path: string;
+    position: string;
+    x: number;
+    y: number; }, 
+    'treeNode'>;
 
 
 interface TreeNodeProps {
@@ -16,14 +29,14 @@ interface TreeNodeProps {
     y: number;
 }
 
-const LearningTreeNode = (props: TreeNodeProps) => {
+const LearningTreeNode = ({ data }: NodeProps<treeNode>) => {
 
-    console.log(props.name);
+    console.log(data.name);
     return (
         <div>
             <Stack spacing={2}>
                 <img src = "logo.png"></img>
-                <h1>{props.name}</h1>
+                <h1>{data.name}</h1>
             </Stack>
         </div>
     );
