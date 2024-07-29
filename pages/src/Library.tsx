69b89ed9653e82fb60f2290f8b0da09aeceed144
devartech <100626221+DevArtech@users.noreply.github.com>
2024-07-28 21:21:12 -0500

    Connect Preview Panel

--- pages/src/Library.tsx
+++ pages/src/Library.tsx
@@ -7,112 +7,132 @@ import ModalItem from "./components/library/ModalItem";
 import { Link, useLocation } from "react-router-dom";
 import { ArrowForward } from "@mui/icons-material";
 import ModalItemPreview from "./components/library/ModalItemPreview";
+import React from "react";
 
 const Library = () => {
+  const [previewedArticle, setPreviewedArticle] = React.useState<string>("");
+
+  function openPreview(articleId: string) {
+    if (previewedArticle === articleId) {
+      setPreviewedArticle("");
+      return true;
+    }
+    setPreviewedArticle(articleId);
+    return false;
+  }
+
+  function hidePreview() {
+    setPreviewedArticle("");
+  }
+
   const rosieItems = [
-    <ModalItem />,
-    <ModalItem />,
-    <ModalItem />,
-    <ModalItem />,
-    <ModalItem />,
-    <ModalItem />,
+    <ModalItem articleId={"test"} openPreview={openPreview} />,
+    <ModalItem articleId={"test2"} openPreview={openPreview} />,
+    <ModalItem articleId={"test3"} openPreview={openPreview} />,
+    <ModalItem articleId={"test4"} openPreview={openPreview} />,
+    <ModalItem articleId={"test5"} openPreview={openPreview} />,
+    <ModalItem articleId={"test6"} openPreview={openPreview} />,
   ];
 
   const location = useLocation();
-  const [query, setQuery] = useState<URLSearchParams>(new URLSearchParams(location.search));
+  const [query, setQuery] = useState<URLSearchParams>(
+    new URLSearchParams(location.search)
+  );
   const [category, setCategory] = useState<number>(1);
   useEffect(() => {
     setQuery(new URLSearchParams(location.search));
-  }, [location.search])
-
-  useEffect(() => {
-    console.log(query);
-  })
+  }, [location.search]);
 
   return (
     <div className="App">
       <nav style={{ display: "flex" }}>
         <LeftPanel query={query} setQuery={setQuery} />
-        <section className="modals" style={{maxWidth: "93.25vw"}}>
-          {query.get("nav") === "Featured" && <div>
-          <Modal
-          title="ROSIE 2024 Finalists"
-          chip={
-            <Chip
-              component={Link}
-              color="primary"
-              label="ROSIE 24'"
-              to="/library?nav=Research&type=ROSIE-24"
-              clickable
-              deleteIcon={<ArrowForward/>}
-              onDelete={() => {}}
-            />
-          }
-          items={rosieItems}
-        />
-        <Modal
-            title="Categories"
-            chip={[
-              <Chip
-                variant={category === 1 ? "filled" : "outlined"}
-                component={Link}
-                color="primary"
-                label="All"
-                onClick={() => setCategory(1)}
-                to="/library?nav=Featured"
-                clickable
-              />,
-              <Chip
-                variant={category === 2 ? "filled" : "outlined"}
-                component={Link}
-                color="primary"
-                label="CNNs"
-                onClick={() => setCategory(2)}
-                to="/library?nav=Featured"
-                clickable
-              />,
-              <Chip
-                variant={category === 3 ? "filled" : "outlined"}
-                component={Link}
-                color="primary"
-                label="Transformers"
-                onClick={() => setCategory(3)}
-                to="/library?nav=Featured"
-                clickable
-              />,
-              <Chip
-                variant={category === 4 ? "filled" : "outlined"}
-                component={Link}
-                color="primary"
-                label="RL"
-                onClick={() => setCategory(4)}
-                to="/library?nav=Featured"
-                clickable
-              />,
-              <Chip
-                variant={category === 5 ? "filled" : "outlined"}
-                component={Link}
-                color="primary"
-                label="Medical"
-                onClick={() => setCategory(5)}
-                to="/library?nav=Featured"
-                clickable
-              />,
-              <Chip
-                variant={category === 6 ? "filled" : "outlined"}
-                component={Link}
-                color="primary"
-                label="Quantum"
-                onClick={() => setCategory(6)}
-                to="/library?nav=Featured"
-                clickable
-              />,
-            ]}
-            items={rosieItems}
-          />
-        </div>}
+        <section className="modals" style={{ maxWidth: "93.25vw" }}>
+          {(query.get("nav") === "Featured" || query.get("nav") === null) && (
+            <div>
+              <Modal
+                title="ROSIE 2024 Finalists"
+                chip={
+                  <Chip
+                    component={Link}
+                    color="primary"
+                    label="ROSIE 24'"
+                    to="/library?nav=Research&type=ROSIE-24"
+                    clickable
+                    deleteIcon={<ArrowForward />}
+                    onDelete={() => {}}
+                  />
+                }
+                items={rosieItems}
+              />
+              <Modal
+                title="Categories"
+                chip={[
+                  <Chip
+                    variant={category === 1 ? "filled" : "outlined"}
+                    component={Link}
+                    color="primary"
+                    label="All"
+                    onClick={() => setCategory(1)}
+                    to="/library?nav=Featured"
+                    clickable
+                  />,
+                  <Chip
+                    variant={category === 2 ? "filled" : "outlined"}
+                    component={Link}
+                    color="primary"
+                    label="CNNs"
+                    onClick={() => setCategory(2)}
+                    to="/library?nav=Featured"
+                    clickable
+                  />,
+                  <Chip
+                    variant={category === 3 ? "filled" : "outlined"}
+                    component={Link}
+                    color="primary"
+                    label="Transformers"
+                    onClick={() => setCategory(3)}
+                    to="/library?nav=Featured"
+                    clickable
+                  />,
+                  <Chip
+                    variant={category === 4 ? "filled" : "outlined"}
+                    component={Link}
+                    color="primary"
+                    label="RL"
+                    onClick={() => setCategory(4)}
+                    to="/library?nav=Featured"
+                    clickable
+                  />,
+                  <Chip
+                    variant={category === 5 ? "filled" : "outlined"}
+                    component={Link}
+                    color="primary"
+                    label="Medical"
+                    onClick={() => setCategory(5)}
+                    to="/library?nav=Featured"
+                    clickable
+                  />,
+                  <Chip
+                    variant={category === 6 ? "filled" : "outlined"}
+                    component={Link}
+                    color="primary"
+                    label="Quantum"
+                    onClick={() => setCategory(6)}
+                    to="/library?nav=Featured"
+                    clickable
+                  />,
+                ]}
+                items={rosieItems}
+              />
+            </div>
+          )}
         </section>
-        <ModalItemPreview/>
+        <ModalItemPreview
+          articleId={previewedArticle}
+          openPreview={openPreview}
+          hidePreview={hidePreview}
+        />
       </nav>
     </div>
   );
--- pages/src/components/library/ModalItem.tsx
+++ pages/src/components/library/ModalItem.tsx
@@ -1,15 +1,26 @@
 import { Button } from "@mui/material";
 import "./assets/library/css/modal.css";
 import tempImage from "./assets/library/images/temp-image.jpg";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface ModalItemProps {
   articleId?: string;
+  openPreview?: (articleId: string) => boolean;
 }
 
 const ModalItem = (props: ModalItemProps) => {
+  const navigate = useNavigate();
+
+  function handleRedict() {
+    if (props.openPreview && props.articleId) {
+      if (props.openPreview(props.articleId)) {
+        navigate(`/library?nav=Articles&article=${props.articleId}`);
+      }
+    }
+  }
+
   return (
-    <Button sx={{ width: "100%" }} component={Link} to={`/library?nav=Articles&article=${props.articleId}`}>
+    <Button sx={{ width: "100%" }} onClick={() => handleRedict()}>
       <div style={{ padding: "1rem" }}>
         <img src={tempImage} alt="Preview" style={{ width: "90%" }}></img>
         <h3 className="modal-item-header">
