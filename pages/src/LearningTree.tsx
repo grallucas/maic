4a91e457bc146c44fb547d5c5a31e31f3a0da993
tygeplater <platert@msoe.edu>
2024-08-01 11:03:14 -0500

    Got base tree node format set

--- pages/src/LearningTree.tsx
+++ pages/src/LearningTree.tsx
@@ -1,5 +1,4 @@
 import logo from "./logo.png";
-import Toolbar from "./components/learning-tree/Toolbar"
 import Tree from "./components/learning-tree/Tree"
 import NavBar from "./components/Navbar";
 
@@ -8,20 +7,6 @@ const LearningTree = () => {
     <div className="App">
       <NavBar page = 'LearningTree'/>
       <Tree />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/LearningTree.js</code> and save to reload LearningTree.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 };
--- pages/src/components/learning-tree/LearningTreeNode.tsx
+++ pages/src/components/learning-tree/LearningTreeNode.tsx
@@ -1,6 +1,11 @@
 import React, { memo } from 'react';
 import { Handle, Position } from '@xyflow/react';
 import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import type { Node, NodeProps } from '@xyflow/react';
 import "./assets/css/learningTreeNode.css";
  
@@ -21,10 +26,30 @@ const LearningTreeNode = ({ data }: NodeProps<treeNode>) => {
     console.log(data.name);
     return (
         <div className = "treeNode">
-            <Stack spacing={0.5}>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image={
+                        window.location.href.includes("3000") ? "/tree-thumbnails/ROSIE Supercomputer.jpg" : "/api/v1/library/Sticker/image"
+                      }
+                    alt="Picture of ROSIE"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Who is ROSIE?
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Rosie is MSOE's supercomputer! Learn more here!
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            {/* <Stack spacing={0.5}>
                 <img src = "logo.png" ></img>
                 <h1>{data.name}</h1>
-            </Stack>
+            </Stack> */}
         </div>
     );
 };
