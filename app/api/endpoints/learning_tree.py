c1d272002fffeb2a5189ef8b2452b8a221ad402e
Brett Storoe <storoeb@msoe.edu>
2024-08-01 19:59:53 -0500

    backend changes

--- app/api/endpoints/learning_tree.py
+++ app/api/endpoints/learning_tree.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
-from backend import LearningTree
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+from learning_tree.backend import LearningTree
 
 router = APIRouter()
 
 
 @router.get("/", description="Learning Tree nodes")
 async def get_learning_tree():
-    return {"reponse": LearningTree("content\\learning-tree-nodes").to_dict()}
+    return {"reponse": LearningTree("learning_tree\\learning-tree-nodes").to_dict()}
