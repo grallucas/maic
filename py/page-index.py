68d346aeeded4d096103bc1cb764e09c0cd0f50e
Tyge Plater <platert@msoe.edu>
2024-10-10 09:01:12 -0500

    Contact US possible url fix

--- py/page-index.py
+++ py/page-index.py
@@ -46,7 +46,7 @@ html(
                     h1('Meet the Eboard!'),
                     img(src="https://maic-fastapi-lambda.s3.amazonaws.com/img/misc/maic_eboard_24.jpg", style='width:100%'),
                     p("A passionate team of MSOE university students dedicated to making artificial intelligence knowledge accessible to all. By strengthening our community partnerships each year, and staying on top of current innovations within the field, they create a platform for learning and innovation, inspiring a future driven by AI's transformative potential."),
-                    a('Contact Us', href='./contact.html', style = 'color: #0099ff; font-weight: bold;'),
+                    a('Contact Us', href='./Contact.html', style = 'color: #0099ff; font-weight: bold;'),
                     style='max-width: 400px;'
                 ),
                 div(
