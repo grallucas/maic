b8e7aab98c16c90b901b230d09d9ed5dc837b508
Andy Dao <andydao2020@gmail.com>
2024-12-06 16:43:33 -0600

    weird stuff that should go in gitignore??

--- pages/src/components/library/LeftPanel.tsx
+++ pages/src/components/library/LeftPanel.tsx
@@ -7,7 +7,6 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import Movie from "@mui/icons-material/Movie";
 import Favorite from "@mui/icons-material/Favorite";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
-import LogoDevIcon from '@mui/icons-material/LogoDev';
 import HelpIcon from "@mui/icons-material/Help";
 import { Link } from "react-router-dom";
 
@@ -116,7 +115,7 @@ const LeftPanel = (props: LeftPanelProps) => {
         <Button component={Link} to="/library?nav=Videos" startIcon={<Movie />}>
           Videos
         </Button>
-        <Button component={Link} to="/library?nav=Competitions" startIcon={<LogoDevIcon />}>
+        <Button component={Link} to="/library?nav=Competitions" startIcon={<EmojiEventsIcon />}>
           Competitions
         </Button>
         {/* <Button
